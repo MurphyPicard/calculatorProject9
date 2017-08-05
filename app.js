d0b434aee2800feeab13699d5224374113f4e743
Ara <ayaghsizian@gmail.com>
2017-08-05 12:23:10 -0400

    basic calc functions work designing calc lookalike

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("js is connected");
+// handlers we will use for the calculations
+var firstNumber = 0;
+var secondNum = 0;
+
+function getNum(){
+  firstNumber = $('#firstNum').val();
+  secondNumber = $('#secondNum').val();
+}
+
+var handlers = {
+
+  add: function(){
+    getNum();
+    var sum = parseInt(firstNumber) + parseInt(secondNumber);
+    $('#answer').text(sum);
+  },
+
+  subtract: function(){
+    getNum();
+    var diff = parseInt(firstNumber) - parseInt(secondNumber);
+    $('#answer').text(diff);
+  },
+
+  multiply: function(){
+    getNum();
+    var product = parseInt(firstNumber) * parseInt(secondNumber);
+    $('#answer').text(product);
+  },
+
+  divide: function(){
+    getNum();
+    var quotient = parseInt(firstNumber) / parseInt(secondNumber);
+    $('#answer').text(quotient);
+  }
+}
+
+// style methods
