8cd979a61c7624937562af3e5cc89a0fb3d45f33
Ara <ayaghsizian@gmail.com>
2017-08-05 13:47:55 -0400

    working on pressing 0 button functionality

--- app.js
+++ app.js
@@ -7,31 +7,103 @@ function getNum(){
   secondNumber = $('#secondNum').val();
 }
 
+// for top section
 var handlers = {
 
   add: function(){
     getNum();
     var sum = parseInt(firstNumber) + parseInt(secondNumber);
-    $('#answer').text(sum);
+    $('#answer').text("Answer: "+ sum);
   },
 
   subtract: function(){
     getNum();
     var diff = parseInt(firstNumber) - parseInt(secondNumber);
-    $('#answer').text(diff);
+    $('#answer').text("Answer: "+ diff);
   },
 
   multiply: function(){
     getNum();
     var product = parseInt(firstNumber) * parseInt(secondNumber);
-    $('#answer').text(product);
+    $('#answer').text("Answer: "+ product);
   },
 
   divide: function(){
     getNum();
     var quotient = parseInt(firstNumber) / parseInt(secondNumber);
-    $('#answer').text(quotient);
+    $('#answer').text("Answer: "+ quotient);
   }
 }
 
-// style methods
+var currentNumber = '';
+var userArray = [];
+$('#zero').click(function(){
+  console.log("clicked 0");
+
+  if(currentNumber != ''){
+    currentNumber += "0";
+    $('.display').text(currentNumber);
+  }
+  else if(currentNumber == ''){
+    $('.display').text('0');
+  }
+
+
+
+  console.log(currentNumber);
+  $('.display').text(currentNumber);
+});
+$('#one').click(function(){
+  console.log("clicked 1");
+  currentNumber += "1";
+  console.log(currentNumber);
+  $('.display').text(currentNumber);
+});
+$('#two').click(function(){
+  console.log("clicked 2");
+  currentNumber += "2";
+  console.log(currentNumber);
+  $('.display').text(currentNumber);
+});
+$('#three').click(function(){
+  console.log("clicked 3");
+  currentNumber += "3";
+  console.log(currentNumber);
+  $('.display').text(currentNumber);
+});
+$('#four').click(function(){
+  console.log("clicked 4");
+  currentNumber += "4";
+  console.log(currentNumber);
+  $('.display').text(currentNumber);
+});
+$('#five').click(function(){
+  console.log("clicked 5");
+  currentNumber += "5";
+  console.log(currentNumber);
+  $('.display').text(currentNumber);
+});
+$('#six').click(function(){
+  console.log("clicked 6");
+  currentNumber += "6";
+  console.log(currentNumber);
+  $('.display').text(currentNumber);
+});
+$('#seven').click(function(){
+  console.log("clicked 7");
+  currentNumber += "7";
+  console.log(currentNumber);
+  $('.display').text(currentNumber);
+});
+$('#eight').click(function(){
+  console.log("clicked 8");
+  currentNumber += "8";
+  console.log(currentNumber);
+  $('.display').text(currentNumber);
+});
+$('#nine').click(function(){
+  console.log("clicked 9");
+  currentNumber += "9";
+  console.log(currentNumber);
+  $('.display').text(currentNumber);
+});
